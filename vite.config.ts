aa51af836eb7760df29936cd63bb6759b823ea1c
EmmanuelJCA <emmanuelcanate@gmail.com>
2024-01-10 13:42:29 -0400

    feat: vite proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,20 @@ import * as path from 'path';
 
 // ----------------------------------------------------------------------
 
+const apiUrl = process.env.API_URL;
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: apiUrl,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 });
