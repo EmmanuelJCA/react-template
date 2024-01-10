fb6d3efa10522445fa7ef5ff8c08a6d051a2f560
EmmanuelJCA <emmanuelcanate@gmail.com>
2024-01-10 19:35:00 -0400

    add: dependency react helmet async

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 
 import App from './App.tsx';
 
@@ -10,8 +11,12 @@ const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Suspense>
+          <App />
+        </Suspense>
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>,
 );
