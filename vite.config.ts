f0d1589649a0c0d7769fd0385ac2edd376cc4e07
EmmanuelJCA <emmanuelcanate@gmail.com>
2024-01-17 15:36:43 -0400

    fix: env variables undefined in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
 // ----------------------------------------------------------------------
 
-const apiUrl = process.env.API_URL;
-
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: apiUrl,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
     }
   }
