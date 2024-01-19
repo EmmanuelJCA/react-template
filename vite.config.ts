a8963bd8598bcd3d2a3fcd1bf0627e42dab7459a
Emmanuel Cañate <emmanuelcanate@gmail.com>
2024-01-19 15:13:51 -0400

    feat: pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import * as path from 'path';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 import { loadEnv, defineConfig } from 'vite';
 
 // ----------------------------------------------------------------------
@@ -7,7 +8,39 @@ import { loadEnv, defineConfig } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        manifest: {
+          name: 'App',
+          description: 'App description',
+          theme_color: '#000000',
+          background_color: '#ffffff',
+          display: 'standalone',
+          lang: 'en-US',
+          start_url: '/',
+          icons: [
+            {
+              src: 'android-chrome-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+              purpose: 'any',
+            },
+            {
+              src: 'android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+      }),
+    ],
     resolve: {
       alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
     },
